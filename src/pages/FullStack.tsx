4ad1e0d9122aa1224caaf5bf6c0ca94658e17c3f
Santiago C <3glbbv5pg@mozmail.com>
2023-01-20 17:01:29 -0300

    Section 5 on FullStack done out of Labs

--- src/pages/FullStack.tsx
+++ src/pages/FullStack.tsx
@@ -4,6 +4,7 @@ import Section1 from '../components/Sections/Section1';
 import Section2 from '../components/Sections/Section2';
 import Section3 from '../components/Sections/Section3';
 import { Section4 } from '../components/Sections/Section4';
+import { Section5 } from '../components/Sections/Section5';
 
 import './../scripts/scripts';
 import './../../node_modules/bootstrap/dist/css/bootstrap.css';
@@ -29,6 +30,7 @@ const FullStack = () => {
                 </Section2>
                 <Section3 />
                 <Section4 />
+                <Section5 />
 
             </Layout>
         </>
