dc35c6ea79dd09cc561fe24489d48ea18655f3b4
Santiago C <3glbbv5pg@mozmail.com>
2023-01-19 22:39:46 -0300

    Solved Conflicts
    # Conflicts:
    #	src/components/Sections/Section2.tsx

--- src/components/Sections/Section2.tsx
+++ src/components/Sections/Section2.tsx
@@ -64,14 +64,55 @@ class Section2 extends Component {
     render() {
         const { activeItem } = this.state
 
-const Section1 = () => {
-    return (
-        <React.Fragment>
-    
-            
-        </React.Fragment>
+        return (
+            <React.Fragment>
+                <br />
+                <br />
 
-    );
+
+                <Menu pointing secondary>
+                    <Menu.Item
+                        name='Desarrollador Web 2016'
+
+                        active={activeItem === 'Desarrollador Web 2016'}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name='Frontend con JS 2022'
+
+                        active={activeItem === 'Frontend con JS 2022'}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name='Otros Cursos 2022'
+
+                        active={activeItem === 'Otros Cursos 2022'}
+                        onClick={
+                            this.handleItemClick
+
+                        }
+                    />
+
+                </Menu>
+
+                <Segment id='wd' className=''>
+
+                    <WebDev2016></WebDev2016>
+                </Segment>
+                <Segment id='FrontEnd' className='seesconde'>
+
+                    <FrontEndNextU></FrontEndNextU>
+                </Segment>
+                <Segment id='otros' className='seesconde'>
+
+                    <OtrosNextU></OtrosNextU>
+
+                </Segment>
+                {/* <img src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' /> */}
+
+            </React.Fragment>
+        )
+    }
 }
 
 
