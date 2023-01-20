be578083d226d4c0501889f95aef1992af27c3f1
Santiago C <3glbbv5pg@mozmail.com>
2023-01-20 16:59:15 -0300

    Correction

--- src/components/Sections/Section4.tsx
+++ src/components/Sections/Section4.tsx
@@ -25,40 +25,12 @@ const Section4 = () => {
     return (
 
         <React.Fragment>
-            {/* <Card>
-                <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' wrapped ui={false} />
-                <Card.Content>
-                    <Card.Header>Daniel</Card.Header>
-                    <Card.Meta>Joined in 2016</Card.Meta>
-                    <Card.Description>
-                        Daniel is a comedian living in Nashville.
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <a>
-                        <Icon name='user' />
-                        10 Friends
-                    </a>
-                </Card.Content>
-            </Card>
 
-            <Card>
-                <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' wrapped ui={false} />
-                <Card.Content>
-                    <Card.Header>Daniel</Card.Header>
-                    <Card.Meta>Joined in 2016</Card.Meta>
-                    <Card.Description>
-                        Daniel is a comedian living in Nashville.
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <a>
-                        <Icon name='user' />
-                        10 Friends
-                    </a>
-                </Card.Content>
-            </Card> */}
             <br /> <br />
+            <center>
+            <h2>
+                Sobre quienes me formaron:</h2></center>
+                <br />
             <Grid columns='three' divided centered>
                 <Grid.Row>
 
--- src/pages/Labs.tsx
+++ src/pages/Labs.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Layout from '../components/Layout/Layout';
 import { Section4 } from '../components/Sections/Section4';
+import { Section5 } from '../components/Sections/Section5';
 import Section3 from '../components/Sections/Section3';
 import '../scripts/scripts';
 import './../../node_modules/bootstrap/dist/css/bootstrap.css';
@@ -8,8 +9,9 @@ const FullStack = () => {
     return (
         <>
             <Layout>
-                <Section3 />
+              
                 <Section4 />
+                <Section5 />
             </Layout>
         </>
     );
